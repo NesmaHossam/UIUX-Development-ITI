afbe47a04886805dd8b04488572a68a6458f824c
NesmaHossam <74352321+NesmaHossam@users.noreply.github.com>
2025-01-03 19:12:16 +0200

    Delete Client Side JS/Day3 directory

--- Client Side JS/Day3/Day7/EventObject/Task2/input.js
+++
@@ -1,6 +0,0 @@
-// 2.  Make your own input type number that accepts numbers only. “using event object”
-
-var input = document.querySelector("input");
-input.addEventListener("input", function (event) {
-  input.value = input.value.replace(/[^0-9]/g, "");
-});
